94e54106ba9c189225c23878ceb7674ee2bbb4c3
이승환 <seaung13@gmail.com>
2025-08-04 21:19:54 +0900

    test: Umpire(심판)의 볼 카운트 기능 테스트 추가 (Red)

--- src/test/java/study/UmpireTest.java
+++ src/test/java/study/UmpireTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UmpireTest {
 
     private Umpire umpire;
-    
+
     @BeforeEach
     void setUp() {
         umpire = new Umpire();
@@ -71,4 +71,73 @@ public class UmpireTest {
         assertThat(strikeCount).isEqualTo(3);
     }
 
+    @DisplayName("숫자가 일치하고 위치까지 일치하면, 볼이 아닌 스트라이크이다.")
+    @Test
+    void ball_zero_but_three_strike() {
+        // given
+        int[] userInputs = {1, 2, 3};
+        int[] targets = {1, 2, 3};
+
+        // when
+        int ballCount = umpire.ball(userInputs, targets);
+
+        // then
+        assertThat(ballCount).isEqualTo(0);
+    }
+
+    @DisplayName("숫자가 모두 일치하지 않으면, 0볼 이다.")
+    @Test
+    void ball_zero() {
+        // given
+        int[] userInputs = {4, 5, 6};
+        int[] targets = {1, 2, 3};
+
+        // when
+        int ballCount = umpire.ball(userInputs, targets);
+
+        // then
+        assertThat(ballCount).isEqualTo(0);
+    }
+
+    @DisplayName("하나의 숫자가 일치하지만 위치가 일치하지 않으면, 1볼 이다.")
+    @Test
+    void ball_one() {
+        // given
+        int[] userInputs = {3, 5, 6};
+        int[] targets = {1, 2, 3};
+
+        // when
+        int ballCount = umpire.ball(userInputs, targets);
+
+        // then
+        assertThat(ballCount).isEqualTo(1);
+    }
+
+    @DisplayName("두 개의 숫자가 일치하지만 위치가 일치하지 않으면, 2볼 이다.")
+    @Test
+    void ball_two() {
+        // given
+        int[] userInputs = {3, 1, 6};
+        int[] targets = {1, 2, 3};
+
+        // when
+        int ballCount = umpire.ball(userInputs, targets);
+
+        // then
+        assertThat(ballCount).isEqualTo(2);
+    }
+
+    @DisplayName("세 개의 숫자가 일치하지만 위치가 일치하지 않으면, 3볼 이다.")
+    @Test
+    void ball_three() {
+        // given
+        int[] userInputs = {3, 1, 2};
+        int[] targets = {1, 2, 3};
+
+        // when
+        int ballCount = umpire.ball(userInputs, targets);
+
+        // then
+        assertThat(ballCount).isEqualTo(3);
+    }
 }
