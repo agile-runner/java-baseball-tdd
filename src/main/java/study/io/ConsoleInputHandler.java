ab5a213fc0d428fb0e1fa048717db469e9e435bc
eun0 <shinely12@gmail.com>
2025-08-09 01:48:57 +0900

    feat(ConsoleInputHandler): 값을 미리 입력한 Scanner를 매개변수로 받는 테스트용 메서드 추가
    - (테스트하기 어려운 영역인) 입력을 처리하기 위해 Scanner를 넘겨 받음

--- src/main/java/study/io/ConsoleInputHandler.java
+++ src/main/java/study/io/ConsoleInputHandler.java
@@ -26,4 +26,23 @@ public class ConsoleInputHandler implements InputHandler {
         return inputs;
     }
 
+    @Override
+    public int[] getDigitsFromUser(Scanner scanner) {
+        String userInput = scanner.nextLine();
+        if (userInput.length() != BASE_LENGTH) {
+            throw new IllegalArgumentException("사용자 입력은 세 자리만 가능합니다.");
+        }
+
+        int[] inputs = new int[userInput.length()];
+        for (int i = 0; i < userInput.length(); i++) {
+            char characterFromUserInput = userInput.charAt(i);
+            if (!Character.isDigit(characterFromUserInput)) {
+                throw new IllegalArgumentException("사용자 입력은 숫자만 가능합니다.");
+            }
+            inputs[i] = characterFromUserInput - '0';
+        }
+
+        return inputs;
+    }
+
 }
