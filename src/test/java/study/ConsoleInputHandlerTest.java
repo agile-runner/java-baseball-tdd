a29f1006425e62e982728fa1973e5085b9220640
eun0 <shinely12@gmail.com>
2025-08-09 01:25:59 +0900

    fix(ConsoleInputHandlerTest): 값을 미리 입력한 Scanner를 매개변수로 넘기도록 수정
    - 입력 값에 개행 문자 추가
    - 의도 전달을 위해 예외 메시지와 DisplayName의 '세자리 숫자'를 '세 자리'로 수정

--- src/test/java/study/ConsoleInputHandlerTest.java
+++ src/test/java/study/ConsoleInputHandlerTest.java
@@ -7,12 +7,14 @@ import study.io.ConsoleInputHandler;
 import study.io.InputHandler;
 
 import java.io.ByteArrayInputStream;
+import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ConsoleInputHandlerTest {
 
+    private Scanner scanner;
     private InputHandler inputHandler;
 
     @BeforeEach
@@ -24,52 +26,58 @@ class ConsoleInputHandlerTest {
     @Test
     void getNumberFromUser_3digit() {
         // given
-        String input = "123";
+        String input = "123\n";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
+        scanner = new Scanner(System.in);
 
         // when
-        int[] digits = inputHandler.getDigitsFromUser();
+        int[] digits = inputHandler.getDigitsFromUser(scanner);
 
         // then
         int[] expected = {1, 2, 3};
         assertThat(digits).isEqualTo(expected);
     }
-    @DisplayName("입력은 반드시 세자리 수여야 컴퓨터와 비교 가능하다.(1)")
+
+    @DisplayName("입력은 반드시 세 자리여야 컴퓨터와 비교 가능하다.(1)")
     @Test
     void getNumberFromUser_4digit() {
         // given
-        String input = "1234";
+        String input = "1234\n";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
+        scanner = new Scanner(System.in);
 
         // when & then
-        assertThatThrownBy(() -> inputHandler.getDigitsFromUser())
+        assertThatThrownBy(() -> inputHandler.getDigitsFromUser(scanner))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("사용자 입력은 세자리 수만 가능합니다.");
+                .hasMessage("사용자 입력은 세 자리만 가능합니다.");
     }
 
-    @DisplayName("입력은 반드시 세자리 수여야 컴퓨터와 비교 가능하다.(2)")
+    @DisplayName("입력은 반드시 세 자리여야 컴퓨터와 비교 가능하다.(2)")
     @Test
     void getNumberFromUser_2digit() {
         // given
-        String input = "12";
+        String input = "12\n";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
+        scanner = new Scanner(System.in);
 
         // when & then
-        assertThatThrownBy(() -> inputHandler.getDigitsFromUser())
+        assertThatThrownBy(() -> inputHandler.getDigitsFromUser(scanner))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("사용자 입력은 세자리 수만 가능합니다.");
+                .hasMessage("사용자 입력은 세 자리만 가능합니다.");
     }
 
     @DisplayName("입력은 반드시 숫자여야 컴퓨터와 비교 가능하다.")
     @Test
     void getNumberFromUser_string() {
         // given
-        String input = "A23";
+        String input = "A23\n";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
+        scanner = new Scanner(System.in);
 
         // when & then
-        assertThatThrownBy(() -> inputHandler.getDigitsFromUser())
+        assertThatThrownBy(() -> inputHandler.getDigitsFromUser(scanner))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사용자 입력은 숫자만 가능합니다.");
     }
+
 }
