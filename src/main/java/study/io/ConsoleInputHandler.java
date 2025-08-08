1ebaf681fdf14e230b709bac591b163be668551c
eun0 <shinely12@gmail.com>
2025-08-09 00:58:30 +0900

    feat(ConsoleInputHandler-getDigitsFromUser): 사용자 입력 기능 추가 (Green but Red)
    - 입력이 세 자리가 아니면 예외가 발생한다
    - 입력이 숫자가 아니면 예외가 발생한다
    - 입력 조건이 '서로 다른' 숫자인데, 테스트 코드에서 누락했음!!
    - [java.util.NoSuchElementException: No line found]
      - 사용자 입력을 만들었지만 테스트에서 실제로 사용하지 않아서 예외 발생

---
+++ src/main/java/study/io/ConsoleInputHandler.java
@@ -0,0 +1,29 @@
+package study.io;
+
+import java.util.Scanner;
+
+public class ConsoleInputHandler implements InputHandler {
+
+    private static final int BASE_LENGTH = 3;
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    @Override
+    public int[] getDigitsFromUser() {
+        String userInput = SCANNER.nextLine();
+        if (userInput.length() != BASE_LENGTH) {
+            throw new IllegalArgumentException("사용자 입력은 세 자리만 가능합니다.");
+        }
+
+        int[] inputs = new int[userInput.length()];
+        for (int i = 0; i < userInput.length(); i++) {
+            char characterFromUserInput = userInput.charAt(i);
+            if (!Character.isDigit(characterFromUserInput)) {
+                throw new IllegalArgumentException("사용자 입력은 숫자만 가능합니다.");
+            }
+            inputs[i] = characterFromUserInput - '0';
+        }
+
+        return inputs;
+    }
+
+}
