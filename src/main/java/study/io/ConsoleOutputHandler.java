abff1d35206ccd44cc1cfea6a3a94f966a008296
이승환 <seaung13@gmail.com>
2025-08-11 21:16:56 +0900

    feat(ConsoleOutputHandler): OutputHandler 구현체 추가

---
+++ src/main/java/study/io/ConsoleOutputHandler.java
@@ -0,0 +1,23 @@
+package study.io;
+
+public class ConsoleOutputHandler implements OutputHandler {
+    @Override
+    public void showMessageForUserInput() {
+
+    }
+
+    @Override
+    public void showUmpireCall(int ball, int strike) {
+
+    }
+
+    @Override
+    public void showRestartOrQuitOptions() {
+
+    }
+
+    @Override
+    public void showGameWinningMessage() {
+
+    }
+}
