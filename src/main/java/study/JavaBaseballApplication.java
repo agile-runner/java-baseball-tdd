cb6b866d11ab1a8c04cb3cf99aa0d0ee5220cbc7
eun0 <shinely12@gmail.com>
2025-08-11 23:51:14 +0900

    feat(JavaBaseballApplication): 게임 애플리케이션 클래스 추가

---
+++ src/main/java/study/JavaBaseballApplication.java
@@ -0,0 +1,9 @@
+package study;
+
+public class JavaBaseballApplication {
+    public static void main(String[] args) {
+        JavaBaseball game = new JavaBaseball();
+
+        game.run();
+    }
+}
