0b96d311a0d8597effebcecff1462839d399decc
이승환 <seaung13@gmail.com>
2025-08-14 21:30:34 +0900

    refactor(Computer-generateRandomNumber): depth를 1로 줄임
    - 가독성 향상을 위해 변수 선언 위치 변경

--- src/main/java/study/Computer.java
+++ src/main/java/study/Computer.java
@@ -7,23 +7,18 @@ public class Computer {
     public int[] generateRandomNumber() {
         Random random = new Random();
         int random1 = random.nextInt(9) + 1;
-        int random2 = 0;
-        int random3 = 0;
-        while (true) {
+
+        int random2 = random.nextInt(9) + 1;
+        while (random2 == random1) {
             random2 = random.nextInt(9) + 1;
-            if (random2 != random1) {
-                break;
-            }
         }
 
-        while (true) {
+        int random3 = random.nextInt(9) + 1;
+        while ((random3 == random1) || (random3 == random2)) {
             random3 = random.nextInt(9) + 1;
-            if ((random3 != random1) && (random3 != random2)) {
-                break;
-            }
         }
 
-        return new int[] {random1, random2, random3};
+        return new int[]{random1, random2, random3};
     }
 
 }
